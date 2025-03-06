0a9cbbb060e962d0aea73163e7009aae2360cfc6
Nitish kumar <nitish01011501kumar@gmail.com>
2025-03-06 07:53:54 +0530

    connected to MongoDb and added env file

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/database.js';
+
 const app = express();
+import dotenv from 'dotenv'
 
 app.use("/api/auth",authRoutes);
+dotenv.config();
 
-
-const port = 5000;
+const port = process.env.PORT;
 app.listen(port,()=>{
-    console.log(`App is running at PORT : ${port}`)
+    console.log(`App is running at PORT : ${port}`);
+    connectDB();
 })
