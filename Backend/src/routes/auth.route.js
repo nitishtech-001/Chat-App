41d2cfa410af4464221c42cac04781af6ddb95ed
Nitish kumar <nitish01011501kumar@gmail.com>
2025-03-09 09:47:41 +0530

    remove the unused import and extra lines

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,12 +1,9 @@
 
 import express from "express";
 import { signup, login} from "../controllers/auth.controller.js";
-import { logout, updateProfile } from "../controllers/user.controler.js";
-import verifyToken from "../middleware/verifytoken.js";
 
 const router = express.Router();
 router.post("/signup",signup);
-
 router.post("/login",login);
 
 export default router;
