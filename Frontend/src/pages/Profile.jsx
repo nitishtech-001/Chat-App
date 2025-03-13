60778921153478d0bf7bdb1ced7974eed790dfd9
Nitish kumar <nitish01011501kumar@gmail.com>
2025-03-13 22:21:37 +0530

    small mistake in max-auto to mx-auto

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -49,7 +49,7 @@ export default function Profile() {
   }
   return (
     <div className="h-screen pt-20">
-      <div className="max-w-2xl max-auto p-4 py-8">
+      <div className="max-w-2xl mx-auto p-4 py-8">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
             <h1 className="text-2xl font-semibold">Profile</h1>
