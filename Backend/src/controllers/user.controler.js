30637bd16c1ea3af23db6db0c1b37862ca65395b
Nitish kumar <nitish01011501kumar@gmail.com>
2025-03-13 15:47:02 +0530

    updating the userupdate controller so user can only chnage avatar and password

--- Backend/src/controllers/user.controler.js
+++ Backend/src/controllers/user.controler.js
@@ -42,8 +42,6 @@ export const updateProfile = async (req, res, next) => {
     const updateUser = await User.findByIdAndUpdate(req.params.id,
         {
             $set:{
-                username : req.body.username,
-                email : req.body.email,
                 password : req.body.password,
                 avatar : req.body.avatar
             }
